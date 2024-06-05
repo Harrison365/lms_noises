5f31e9f723eaaa74bfe70214711a325507d61907
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-06-05 12:59:18 +0100

    readme updated and noise folder added

--- noiseServer.js
+++ noiseServer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use("/mp3", express.static("./splosh.mp3"));
+app.use("/mp3", express.static("./noises/splosh.mp3"));
 
 app.listen(8080, () => {
   console.log("I will serve your browser noises on port 8080!");
