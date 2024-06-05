b526fdec4dd7105634abbdde81650eb6514cb581
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-06-05 11:53:38 +0100

    new noise

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-yo
+# LMS Noises
+
+Hello. This is a repo to get your LMS helpdesk playing a noise when you have pending nc-helps.
+
+## How to use
+
+1. Clone this repo
--- noiseServer.js
+++ noiseServer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use("/mp3", express.static("./bbc_mp3.mp3"));
+app.use("/mp3", express.static("./splosh.mp3"));
 
 app.listen(8080, () => {
   console.log("I will serve your browser noises on port 8080!");
